af07a2f2bfdd832765f013d8841a5e12afc36349
75-75 <75.75akane@gmail.com>
2022-10-12 13:51:26 +0900

    section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -2,5 +2,15 @@
 function setup(){
   for(let i = 0; i < 10; i++){
     // BLANK[1]
+    noFill();
+  let x=i*8+8
+   
+    if(i<5){
+      stroke(0,0,255);
+    }
+    else{
+      stroke(255,0,0);
+    }
+     ellipse(50,50,x);
   }
 }
