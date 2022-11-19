812d8a6a82866867b126f0675a91def0d2e71e07
75-75 <75.75akane@gmail.com>
2022-11-20 01:48:08 +0900

    finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,13 +1,77 @@
 // 最終課題を制作しよう
 
-function setup(){
-  createCanvas(windowWidth, windowHeight);
+let house=[];
+
+const g = 1;     
+const jump = 20; 
+const ground = 20;
+const size = 20;
+
+let x, y, vy, gy;
+
+function setup() {
+  createCanvas(500, 500);
+ 
+  
+  x = 10;
+  y = height - ground - size / 2;
+  vy = 0;
+  gy = height - ground;
+  
 }
 
-function draw(){
-  background(160, 192, 255);
+function building(x,y,a,b){
+  house.push({positionX:x, positionY:y, sizeX:a, sizeY:b});
+  fill(255);
+  stroke(0,0,0);
+  rect(x,y-b,a,b);
+  rect(x+a,y-(b+20),a,b+20);
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
+
+function draw() {
+  background(117,227,255)
+  line(0, gy, width, gy); 
+   
+  building(50,gy,30,80)
+  building(150,gy,40,60)
+  building(250,gy,10,180)
+  building(290,gy,15,150)
+  building(350,gy,40,50)
+  ellipse(x, y, size, size)
+  let tempoX=x
+  let tempoY=y
+  
+ 
+  y += vy;
+  
+  if(y < height - ground - size / 2){ 
+    vy += 0.5*g; 
+  }
+  else{
+    vy = 0;
+    y = height - ground - size / 2;
+  }
+ 
+if(keyIsDown(LEFT_ARROW)){x-=2;}
+if(keyIsDown(RIGHT_ARROW)){x+=2;}
+
+  for(let i =0; i<house.length; i++){
+    if(x>house[i].positionX && x<house[i].positionX+house[i].sizeX && y>house[i].positionY-house[i].sizeY ){
+      x=tempoX
+      y=tempoY
+  }
+    if(x>house[i].positionX+house[i].sizeX && x<house[i].positionX+2*house[i].sizeX && y>house[i].positionY-(house[i].sizeY+20) ){
+      x=tempoX
+      y=tempoY
+  }
+}}
+
+function keyPressed(){
+   if(keyCode === UP_ARROW){
+    if(y>1/2*height){vy=-0.5*jump;}
+    else{vy+= g}}
 }
+
+
+
